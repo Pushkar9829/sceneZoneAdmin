9e6e4da22573be507371c199e559d18b6f542b83
pushkar9829 <hraj6398@gmail.com>
2026-03-07 23:56:50 +0530

    build

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@
 import { ApiError } from './exceptions'
 import { cookies } from 'next/headers'
 
-// const BACKEND_URL = 'https://app.scenezone.in/api'
+// const BACKEND_sURL = 'https://app.scenezone.in/api'
 const BACKEND_URL = 'https://app.scenezone.in/api'
 
 export async function fetchApi<T>(
